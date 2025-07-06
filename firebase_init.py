c4967f40b0fff47afc6465b2958bac3861ed94be
Anindo Paul <anindo046@gmail.com>
2025-07-06 15:57:40 +0600

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,9 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import json
+import streamlit as st
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_key.json")
+        firebase_key = json.loads(st.secrets["firebase_key"])
+        cred = credentials.Certificate(firebase_key)
         firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://YOUR_PROJECT_ID.firebaseio.com/'  # 🔁 Replace with your actual DB URL
+            'databaseURL': st.secrets["databaseURL"]
+        })
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+import streamlit as st
+
+def init_firebase():
+    if not firebase_admin._apps:
+        firebase_key = json.loads(st.secrets["firebase_key"])
+        cred = credentials.Certificate(firebase_key)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': st.secrets["databaseURL"]
         })
